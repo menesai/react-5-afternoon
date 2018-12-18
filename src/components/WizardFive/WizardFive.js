7241a2f02d89c4de5ff6792eea7819187fe007ca
Uriel Meneses <menesai@Uriels-MBP.5-A-Rm509>
2018-12-17 19:01:02 -0600

    commit

--- src/components/WizardFive/WizardFive.js
+++ src/components/WizardFive/WizardFive.js
@@ -1,22 +1,45 @@
-import React,  { Component } from 'react';
-import { Link } from 'react-router-dom';
-
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+import { connect } from "react-redux"
+import { agentFound } from "../../ducks/reducer"
 class WizardFive extends Component {
-
-    render(){
-        return(
-            <div className="parent-div">
-                    <div className="vert-align">    
-
-                    <p>Are you currently working with a real estate agent?</p> <br />
-                    <div className="row">
-                        <Link to="/wSix"><button onClick={this.props.realEstateAgentTrue}>Yes</button></Link>
-                        <Link to="/wSix"><button onClick={this.props.realEstateAgentFalse}>No </button></Link>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className='parent-div'>
+        <div className='vert-align'>
+          <p>Are you currently working with a real estate agent?</p> <br />
+          <div className='row'>
+            <Link to='/wSix'>
+              <button
+                onClick={e => {
+                  this.props.agentFound("true")
+                }}
+              >
+                Yes
+              </button>
+            </Link>
+            <Link to='/wSix'>
+              <button
+                onClick={e => {
+                  this.props.agentFound("false")
+                }}
+              >
+                No{" "}
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default WizardFive;
+const mapStateToProps = state => {
+  const { realEstateAgent } = state
+  return {
+    realEstateAgent
+  }
+}
+export default connect(
+  mapStateToProps,
+  { agentFound }
+)(WizardFive)
